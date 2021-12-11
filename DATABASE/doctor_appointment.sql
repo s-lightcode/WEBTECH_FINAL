9c2b580872cc337aa0b3c869e1af93ab5b4a7891
s-lightcode <57291507+s-lightcode@users.noreply.github.com>
2021-12-11 18:57:30 +0000

    Update doctor_appointment.sql

--- DATABASE/doctor_appointment.sql
+++ DATABASE/doctor_appointment.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2019 at 08:15 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -31,7 +40,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `updateDate`) VALUES
-(1, 'admin', 'password', '28-12-2016 11:42:05 AM');
+(1, 'admin', 'Test@12345', '28-12-2016 11:42:05 AM');
 
 -- --------------------------------------------------------
 
@@ -44,22 +53,20 @@ CREATE TABLE `appointment` (
   `doctorSpecialization` varchar(255) DEFAULT NULL,
   `doctorId` int(11) DEFAULT NULL,
   `userId` int(11) DEFAULT NULL,
-  `consultationFees` int(11) DEFAULT NULL,
+  `consultancyFees` int(11) DEFAULT NULL,
   `appointmentDate` varchar(255) DEFAULT NULL,
   `appointmentTime` varchar(255) DEFAULT NULL,
   `postingDate` timestamp NULL DEFAULT current_timestamp(),
   `userStatus` int(11) DEFAULT NULL,
-  `doctorStatus` int(11) DEFAULT NULL,
-  `updateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `doctorStatus` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultationFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updateDate`) VALUES
+INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updateDate`) VALUES
 (3, 'Demo test', 7, 6, 600, '2019-06-29', '9:15 AM', '2019-06-23 18:31:28', 1, 0, '0000-00-00 00:00:00'),
-(4, 'Radiologist', 5, 5, 8050, '2019-11-08', '1:00 PM', '2019-11-05 10:28:54', 1, 1, '0000-00-00 00:00:00'),
 (5, 'Dermatologist', 9, 7, 500, '2019-11-30', '5:30 PM', '2019-11-10 18:41:34', 1, 0, '2019-11-10 18:48:30');
 
 -- --------------------------------------------------------
@@ -77,14 +84,16 @@ CREATE TABLE `doctors` (
   `contactno` bigint(11) DEFAULT NULL,
   `docEmail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `doctors`
 --
 
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updateDate`) VALUES
+(1, 'Dentist', 'James', 'Accra', '500', 8285703354, 'james@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-29 06:25:37', '2021-12-11 09:12:05'),
+(9, 'Dermatologist', 'Adam Quaye', 'Achimota', '500', 1234567890, 'adam.quaye@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-11-10 18:37:47', '2021-12-11 09:12:56');
 
 -- --------------------------------------------------------
 
@@ -111,8 +120,7 @@ CREATE TABLE `doctorslog` (
 CREATE TABLE `doctorspecilization` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -120,17 +128,17 @@ CREATE TABLE `doctorspecilization` (
 --
 
 INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updateDate`) VALUES
-(1, 'General Physician', '2016-12-28 06:37:25', '0000-00-00 00:00:00'),
-(2, 'Virologist', '2016-12-28 06:38:12', '0000-00-00 00:00:00'),
+(1, 'Gynecologist/Obstetrician', '2016-12-28 06:37:25', '0000-00-00 00:00:00'),
+(2, 'General Physician', '2016-12-28 06:38:12', '0000-00-00 00:00:00'),
 (3, 'Dermatologist', '2016-12-28 06:38:48', '0000-00-00 00:00:00'),
-(4, 'Pathologist', '2016-12-28 06:39:26', '0000-00-00 00:00:00'),
-(5, 'Optician', '2016-12-28 06:39:51', '0000-00-00 00:00:00'),
+(4, 'Homeopath', '2016-12-28 06:39:26', '0000-00-00 00:00:00'),
+(5, 'Ayurveda', '2016-12-28 06:39:51', '0000-00-00 00:00:00'),
 (6, 'Dentist', '2016-12-28 06:40:08', '0000-00-00 00:00:00'),
-(7, 'Neuro-surgeon', '2016-12-28 06:41:18', '0000-00-00 00:00:00'),
-(9, 'Phisiotherapist', '2016-12-28 07:37:39', '0000-00-00 00:00:00'),
-(10, 'Psychologist', '2017-01-07 08:07:53', '0000-00-00 00:00:00'),
-(11, 'Radiologist', '2019-06-23 17:51:06', '2019-06-23 17:55:06'),
-(12, 'Gynecologist/Obstetrician', '2019-11-10 18:36:36', '2019-11-10 18:36:50');
+(7, 'Ear-Nose-Throat (Ent) Specialist', '2016-12-28 06:41:18', '0000-00-00 00:00:00'),
+(9, 'Demo test', '2016-12-28 07:37:39', '0000-00-00 00:00:00'),
+(10, 'Bones Specialist demo', '2017-01-07 08:07:53', '0000-00-00 00:00:00'),
+(11, 'Test', '2019-06-23 17:51:06', '2019-06-23 17:55:06'),
+(12, 'Dermatologist', '2019-11-10 18:36:36', '2019-11-10 18:36:50');
 
 -- --------------------------------------------------------
 
@@ -146,15 +154,16 @@ CREATE TABLE `tblcontactus` (
   `message` mediumtext DEFAULT NULL,
   `PostingDate` timestamp NULL DEFAULT current_timestamp(),
   `AdminRemark` mediumtext DEFAULT NULL,
-  `LastupdateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `IsRead` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblcontactus`
 --
+INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `PostingDate`, `AdminRemark`, `LastupdateDate`, `IsRead`) VALUES
+(1, 'demo', 'demo@gmail.com', 2523523522523523, 'I had headaches', '2019-06-29 19:03:08', 'Take paracetamol', '2021-12-11 09:13:52', 1);
 
-
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tblmedicalhistory`
@@ -173,6 +182,17 @@ CREATE TABLE `tblmedicalhistory` (
 
 --
 -- Dumping data for table `tblmedicalhistory`
+--
+
+INSERT INTO `tblmedicalhistory` (`ID`, `PatientID`, `BloodPressure`, `BloodSugar`, `Weight`, `Temperature`, `MedicalPres`, `CreationDate`) VALUES
+(2, 3, '120/185', '80/120', '85 Kg', '101 degree', '#Fever, #BP high\r\n1.Paracetamol\r\n2.jocib tab\r\n', '2019-11-06 04:20:07'),
+(3, 2, '90/120', '92/190', '86 kg', '99 deg', '#Sugar High\r\n1.Petz 30', '2019-11-06 04:31:24'),
+(4, 1, '125/200', '86/120', '56 kg', '98 deg', '# blood pressure is high\r\n1.koil cipla', '2019-11-06 04:52:42'),
+(5, 1, '96/120', '98/120', '57 kg', '102 deg', '#Viral\r\n1.gjgjh-1Ml\r\n2.kjhuiy-2M', '2019-11-06 04:56:55'),
+(6, 4, '90/120', '120', '56', '98 F', '#blood sugar high\r\n#Asthma problem', '2019-11-06 14:38:33'),
+(7, 5, '80/120', '120', '85', '98.6', 'Rx\r\n\r\nAbc tab\r\nxyz Syrup', '2019-11-10 18:50:23');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tblpatient`
@@ -188,14 +208,21 @@ CREATE TABLE `tblpatient` (
   `PatientAdd` mediumtext DEFAULT NULL,
   `PatientAge` int(10) DEFAULT NULL,
   `PatientMedhis` mediumtext DEFAULT NULL,
-  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblpatient`
 --
 
+INSERT INTO `tblpatient` (`ID`, `Docid`, `PatientName`, `PatientContno`, `PatientEmail`, `PatientGender`, `PatientAdd`, `PatientAge`, `PatientMedhis`, `CreationDate`, `updateDate`) VALUES
+(1, 1, 'David Atongo', 4558968789, 'david.atongo@gmail.com', 'Male', 'Madina Redco', 26, 'Diabetic patient', '2019-11-04 21:38:06', '2021-12-11 09:17:43'),
+(2, 5, 'Reginald Atimpo', 9797977979, 'ratimpo@gmail.com', 'Male', 'Ashaley Botwe', 39, 'No', '2019-11-05 10:40:13', '2021-12-11 09:15:23'),
+(3, 7, 'Mantenso Obeng', 9878978798, 'manobeng@gmail.com', 'Female', 'No', 46, 'No', '2019-11-05 10:49:41', '2021-12-11 09:16:01'),
+(4, 7, 'Martin Ole', 9888988989, 'martinole@gmail.com', 'Male', 'Adenta - Oyarifa', 45, 'He is long suffered by asthma', '2019-11-06 14:33:54', '2021-12-11 09:14:45'),
+(5, 9, 'John Dzane', 1234567890, 'john@gmail.com', 'male', 'Test results', 25, 'I am feeling sick in my throat', '2019-11-10 18:49:24', NULL);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `userlog`
@@ -212,10 +239,20 @@ CREATE TABLE `userlog` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(24, 8, 'ajeimam@ymail.com', 0x3a3a3100000000000000000000000000, '2021-12-10 15:58:25', '10-12-2021 10:19:36 PM', 1),
+(25, NULL, 'sean.ofori-addo@ashesi.edu.gh', 0x3a3a3100000000000000000000000000, '2021-12-10 16:49:58', NULL, 0),
+(26, NULL, 'sean.ofori-addo@ashesi.edu.gh', 0x3a3a3100000000000000000000000000, '2021-12-10 16:50:03', NULL, 0),
+(27, NULL, 'oforiaddosean@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-10 16:50:08', NULL, 0),
+(28, NULL, 'oforiaddosean@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-10 16:50:15', NULL, 0),
+(29, 8, 'ajeimam@ymail.com', 0x3a3a3100000000000000000000000000, '2021-12-10 16:51:27', '10-12-2021 10:22:01 PM', 1),
+(30, 8, 'ajeimam@ymail.com', 0x3a3a3100000000000000000000000000, '2021-12-11 08:47:21', NULL, 1);
 
 --
 -- Table structure for table `users`
 --
+
+
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -225,14 +262,17 @@ CREATE TABLE `users` (
   `gender` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `regDate` timestamp NULL DEFAULT current_timestamp(),
-  `updateDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `regDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
+INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `RegistrationDate`, `updateDate`) VALUES
+(2, 'Kwame', 'Adenta\r\n', 'Accra', 'Male', 'kwame@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-30 05:34:39', '2021-12-11 09:18:59'),
+(8, 'Ajei Quarcoo', 'Kitaase', 'Eastern Region', 'male', 'ajeimam@ymail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-12-10 15:58:04', '2021-12-10 16:34:08'),
+(9, 'Ajei', 'Mampong', 'Eastern Region', 'male', 'ajeimam@ymail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-12-11 08:47:02', NULL);
 
 --
 -- Indexes for dumped tables
