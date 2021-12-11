fd22a2b28ec49df0bd574c0474f8274069104891
s-lightcode <57291507+s-lightcode@users.noreply.github.com>
2021-12-11 08:45:45 +0000

    Update forgot-password.php

--- ODMS/forgot-password.php
+++ ODMS/forgot-password.php
@@ -44,7 +44,7 @@ echo "<script>window.location.href ='forgot-password.php'</script>";
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../index.html"><h2> HMS | Patient Password Recovery</h2></a>
+				<a href="../index.html"><h2> Patient Password Recovery</h2></a>
 				</div>
 
 				<div class="box-login">
@@ -86,11 +86,7 @@ echo "<script>window.location.href ='forgot-password.php'</script>";
 						</fieldset>
 					</form>
 
-					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
-					</div>
-			
-				</div>
+					
 
 			</div>
 		</div>
