255d2ed0665411dea19067aeaa518f9bb564f0e6
s-lightcode <57291507+s-lightcode@users.noreply.github.com>
2021-12-11 22:31:29 +0000

    Update config.php

--- ODMS/include/config.php
+++ ODMS/include/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','sean');
+define('DB_USER','root');
 define('DB_PASS' ,'');
 define('DB_NAME', 'doctor_appointment');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
